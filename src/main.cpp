d601953408db44972bd169efaa67c23a6f18d7db
Richard Riley <rileyrg@gmail.com>
2019-09-16 11:33:36 +0200

    renames and clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
 #include<Arduino.h>
 
-int redLED=8;     // set Red LED as “output”
-int yellowLED=7;  // set Yellow LED as “output”
-int greenLED=6;   // set Green LED as “output”
-int redKey=5;       // initialize pin for Red button
-int yellowKey=4;       // initialize pin for Yellow button
-int greenKey=3;       // initialize pin for Green button
-int resetKey=2;    // initialize pin for reset button
+int redLEDPin=8;     // set Red LED as “output”
+int yellowLEDPin=7;  // set Yellow LED as “output”
+int greenLEDPin=6;   // set Green LED as “output”
+int redKeyPin=5;       // initialize pin for Red button
+int yellowKeyPin=4;       // initialize pin for Yellow button
+int greenKeyPin=3;       // initialize pin for Green button
+int resetKeyPin=2;    // initialize pin for reset button
 
-int setLED=redLED;
+int setLED=redLEDPin;
 
 void resetLEDs();
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(redLED,OUTPUT);
-  pinMode(yellowLED,OUTPUT);
-  pinMode(greenLED,OUTPUT);
-  pinMode(redKey,INPUT);
-  pinMode(yellowKey,INPUT);
-  pinMode(greenKey,INPUT);
-  pinMode(resetKey,INPUT);
-  attachInterrupt(digitalPinToInterrupt(resetKey), resetLEDs,RISING);
+  pinMode(redLEDPin,OUTPUT);
+  pinMode(yellowLEDPin,OUTPUT);
+  pinMode(greenLEDPin,OUTPUT);
+  pinMode(redKeyPin,INPUT);
+  pinMode(yellowKeyPin,INPUT);
+  pinMode(greenKeyPin,INPUT);
+  pinMode(resetKeyPin,INPUT);
+  attachInterrupt(digitalPinToInterrupt(resetKeyPin), resetLEDs,RISING);
   if(setLED)
     digitalWrite(setLED,HIGH);
 }
@@ -30,7 +30,7 @@ void setup()
 void loop()         // repeatedly read pins for buttons
 {
   if(setLED==0){
-    setLED=digitalRead(redKey)?redLED:digitalRead(yellowKey)?yellowLED:digitalRead(greenKey)?greenLED:0;
+    setLED=digitalRead(redKeyPin)?redLEDPin:digitalRead(yellowKeyPin)?yellowLEDPin:digitalRead(greenKeyPin)?greenLEDPin:0;
     if(setLED){
       char str[15];
       Serial.println(sprintf(str,"Key pressed: %u",setLED));
